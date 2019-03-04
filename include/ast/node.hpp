1d409a052441aa007d5cb152e579c16df216d8ab
lengwe <wl4817@ic.ac.uk>
2019-03-04 20:27:44 +0000

    fix seg fault

--- include/ast/node.hpp
+++ include/ast/node.hpp
@@ -1,21 +1,70 @@
+#ifndef node_hpp
+#define node_hpp
+
 #include <iostream>
 #include <string>
+
+class Node;
+
+typedef const Node* Nodeptr;
 
 class Node{
 	public:
 		virtual ~Node() {}
 
+		virtual std::string c()const=0;
+
 		virtual void print(std::ostream &dst)const=0;
 
 };
 
+// class translation_unit: public Node{
+//
+// 	private:
+// 		Nodeptr l,r,p;
+//
+// 	public:
+// 		translation_unit(Nodeptr _p) : p(_p) {}
+// 		translation_unit(Nodeptr _l. Nodeptr _r) : l(_l),r(_r) {}
+// }
+
+
 class type_specifier: public Node{
 	private:
-		std::string* type;
+		int type;
 	public:
-		type_specifier(std::string *type_in) : type(type_in) {}
+		type_specifier(int type_in) : type(type_in) {}
+		//~type_specifier(){delete type;}
 		virtual void print(std::ostream &dst) const override{
-			dst << *type;
+			dst << type;
 		}
 
+		std::string c() const override{
+			switch (type) {
+				case 1:
+					return "void";
+				case 2:
+					return "char";
+				case 3:
+					return "short";
+				case 4:
+					return "int";
+				case 5:
+					return "long";
+				case 6:
+					return "float";
+				case 7:
+					return "double";
+				case 8:
+					return "signed";
+				case 9:
+					return "unsigned";
+			}
+		}
+
+		//code-gen python
+		//void python().....
 };
+
+
+#endif
