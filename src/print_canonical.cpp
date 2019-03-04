1ba8469f12cf0e7451f7e56cd9c21dceccde0b5b
lengwe <wl4817@ic.ac.uk>
2019-03-04 23:15:22 +0000

    Add files via upload

---
+++ src/print_canonical.cpp
@@ -0,0 +1,14 @@
+#include "ast.hpp"
+
+int main()
+{
+    //std::cout<<"1"<<std::endl;
+    const Node* ast=parseAST();
+
+    //ast->print(std::cout);
+    //std::cout<<std::endl;
+
+    std::cout<<ast->c();
+
+    return 0;
+}
