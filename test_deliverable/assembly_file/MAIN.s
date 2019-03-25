0cc028a5812ea738242301451035021207ad0e19
Ramon Jutaviriya <ramon15749@gmail.com>
2019-03-25 15:43:19 +0000

    main works

--- test_deliverable/assembly_file/MAIN.s
+++ test_deliverable/assembly_file/MAIN.s
@@ -3,6 +3,7 @@
 
 
 
+addiu , $zero, 0
 .text
 .align 2
 .globl main
@@ -13,7 +14,30 @@ addiu   $sp,$sp,-200
 sw      $fp,192($sp)
 sw			$ra, 196($sp)
 move    $fp,$sp
-addu $2, $zero, 
+sw $a0, 16($fp)
+sw $a1, 20($fp)
+sw $a2, 24($fp)
+sw $a3, 28($fp)
+sw $2, 32($fp)
+sw $3, 36($fp)
+sw $31, 40($fp)
+addiu $a0, $zero, 5
+jal f
+nop
+lw $a0, 16($fp)
+lw $a1, 20($fp)
+lw $a2, 24($fp)
+lw $a3, 28($fp)
+lw $3, 36($fp)
+lw $31, 40($fp)
+move $t0, $2
+lw $2, 32($fp)
+xori $2, $t0, 15
+sltu $2, $2, 1
+andi $2, $2, 0x00ff
+sltu $2 , $2, 1
+andi $2 , $2, 0x00ff
+addu $2, $zero, $2
 j main_end
 main_end:
 move    $sp,$fp
